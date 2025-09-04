53d4c471193cbf7f71ee1867da343ae88aded8ca
Ramprasad <ramprasad6352@gmail.com>
2025-09-04 19:50:18 +0530

    variable questions

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int arr[] = new int[];
+        int n = 123;
+
+        while(n>0){
+            int digit = n%10;
+            n=n/10;
+        }
+
+    }
+}
